ef78645d65e0895223a6f61c5e1d38bde0407f28
wjdsheep15 <wjd15sheep@gmail.com>
2024-04-07 04:24:25 +0900

    Feat: add reviewModal open in introduction

--- src/components/rooms/modal/review-modal.tsx
+++ src/components/rooms/modal/review-modal.tsx
@@ -14,6 +14,7 @@ import Satisfication from '/public/svgIcons/reviewModalSvgs/satisfactionCompared
 import Search from '/public/svgIcons/reviewModalSvgs/search.svg'
 import StarRateGenerator from '@/components/rooms/starIcon/starRateGenerator'
 import Image from 'next/image'
+import DownIcon from '/public/svgIcons/reviewModalSvgs/down.svg'
 
 interface Review {
   reviewId: number
@@ -51,19 +52,13 @@ export default function ReviewModal({
   reviewModalOpen: boolean
   setReviewModalOpen: (newValue: boolean) => void
 }) {
-  const handleOpen = () => {
-    setReviewModalOpen(true)
-  }
-
   const handleClose = () => {
     setReviewModalOpen(false)
   }
 
+  const [onFocusButton, setOnFocusButton] = useState(false)
+  const [onClickSearchMenu, setOnClickSearchMenu] = useState(false)
   const [selectedMenu, setSelectedMenu] = useState('최신순')
-  const menus = ['최신순', '높은 평점순', '낮은 평점순']
-  const handleMenuClick = (menuName: React.SetStateAction<string>) => {
-    setSelectedMenu(menuName)
-  }
 
   const reviewExist = isReviewExist(reviews)
   const initialReviews = reviews.slice(0, 8)
@@ -75,170 +70,179 @@ export default function ReviewModal({
         <Box
           className='border-1 relative left-1/2 top-1/2 h-full w-full
           -translate-x-1/2
-           -translate-y-1/2 transform border-black bg-white
-           p-3 shadow-lg md:mx-3 md:h-[900px]
-           md:w-[90%] md:rounded-2xl
-           lg:h-[900px] lg:w-[1032px] '
+           -translate-y-1/2 transform overflow-hidden border-black bg-white
+           pb-10 shadow-lg md:mx-3
+           md:h-[900px] md:w-[90%]
+           md:rounded-2xl lg:h-[900px]
+           lg:w-[1032px]'
         >
           {/* 닫는 버튼  */}
           <div className='relative flex h-[72px] w-full items-center'>
-            <button className='absolute left-7 text-black' onClick={handleClose}>
+            <button className='absolute left-4 text-black' onClick={handleClose}>
               <CloseIcon />
             </button>
           </div>
 
           {/* 리뷰 및 별점 전체로 묶은 곳 */}
           {/* lg 이전에는 가로로 xl 이후로는 세로로  구역 두개로 묶어서 처리*/}
-          <div className='ml-2 mt-8 max-h-[600px] w-[1150px] overflow-y-auto overflow-x-hidden'>
+          <div className=' flex h-full w-full flex-col overflow-y-auto xl:flex-row xl:justify-center'>
             {/* 별점 및 점수 처리 */}
-            <div>
-              {reviews.length > 2 ? (
-                <div className='flex'>
-                  <Star />
-                  <div className='pl-3 text-3xl font-semibold'>{`${avgScore}`}</div>
+            <div className='p-4 xl:w-[277px]  xl:flex-col xl:p-8'>
+              <div className='xl:h-[37px] xl:w-full'>
+                {reviews.length > 2 ? (
+                  <div className='flex items-center gap-2'>
+                    <Star style={{ width: '24px', height: '24px' }} />
+                    <div className=' text-3xl font-semibold'>{`${avgScore}`}</div>
+                  </div>
+                ) : (
+                  <div className='mb-3'>
+                    <span className='text-base text-mainGray'>
+                      후기가 3개 이상이면 별점이 표시됩니다.
+                    </span>
+                  </div>
+                )}
+              </div>
+              <div className='flex flex h-[128px] w-full items-center justify-center text-xs xl:mt-5 xl:flex-col xl:items-baseline xl:justify-normal xl:gap-3'>
+                <div className=' h-[95px] w-40 flex-col items-center justify-center border-r-2 px-3 xl:w-full xl:border-r-0 xl:px-0'>
+                  <div className='text-xs xl:text-sm'>전체 평점</div>
+                  <div className='flex items-center'>
+                    <span className='text-[10px] text-gray-500 xl:text-black'>5</span>
+                    <div className='ml-2  h-1 w-[94px] max-w-xs rounded-xl bg-gray-200 xl:w-full'></div>
+                  </div>
+                  <div className='flex items-center'>
+                    <span className='text-[10px] text-gray-500 xl:text-black'>4</span>
+                    <div className='ml-2 h-1 w-[94px] max-w-xs rounded-xl bg-gray-200  xl:w-full'></div>
+                  </div>
+                  <div className='flex items-center'>
+                    <span className='text-[10px] text-gray-500 xl:text-black'>3</span>
+                    <div className='ml-2 h-1 w-[94px] max-w-xs rounded-xl bg-gray-200  xl:w-full'></div>
+                  </div>
+                  <div className='flex items-center'>
+                    <span className='text-[10px] text-gray-500 xl:text-black'>2</span>
+                    <div className='ml-2  h-1 w-[94px] max-w-xs rounded-xl bg-gray-200  xl:w-full'></div>
+                  </div>
+                  <div className='flex flex-nowrap items-center'>
+                    <span className='text-[10px] text-gray-500 xl:text-black'>1</span>
+                    <div className='ml-2 h-1 w-[94px] max-w-xs rounded-xl bg-gray-200  xl:w-full'></div>
+                  </div>
                 </div>
-              ) : (
-                <div className='mb-3'>
-                  <span className='text-[14px] text-mainGray'>
-                    후기가 3개 이상이면 별점이 표시됩니다.
-                  </span>
+
+                <div className='relative  h-[95px] w-36 border-r-2 px-3  xl:mt-5 xl:w-full xl:border-b-2 xl:border-r-0 xl:p-5 xl:px-0'>
+                  <span className='w-16 xl:absolute xl:bottom-4 xl:left-10'>청결도</span>
+                  <div className='xl:absolute xl:bottom-4 xl:right-0'>4.8</div>
+
+                  <Cleanliness className='absolute bottom-3' />
                 </div>
-              )}
-            </div>
-            <div className='relative top-12 flex'>
-              <div className='w-44 border-r-2'>
-                <div className=''>전체 평점</div>
-                <ol>
-                  <li>
-                    <div className='flex'>
-                      <div className='text-sm'>5</div>
-                      <div className='ml-4 mt-2 h-1 w-[120px] max-w-xs rounded-xl bg-gray-200'></div>
-                    </div>
-                  </li>
-                  <li>
-                    <div className='flex'>
-                      <div className='text-sm'>4</div>
-                      <div className='ml-4 mt-2 h-1 w-[120px] max-w-xs rounded-xl bg-gray-200'></div>
-                    </div>
-                  </li>
-                  <li>
-                    <div className='flex'>
-                      <div className='text-sm'>3</div>
-                      <div className='ml-4 mt-2 h-1 w-[120px] max-w-xs rounded-xl bg-gray-200'></div>
-                    </div>
-                  </li>
-                  <li>
-                    <div className='flex'>
-                      <div className='text-sm'>2</div>
-                      <div className='ml-4 mt-2 h-1 w-[120px] max-w-xs rounded-xl bg-gray-200'></div>
-                    </div>
-                  </li>
-                  <li>
-                    <div className='flex'>
-                      <div className='text-sm'>1</div>
-                      <div className='ml-4 mt-2 h-1 w-[120px] max-w-xs rounded-xl bg-gray-200'></div>
-                    </div>
-                  </li>
-                </ol>
-              </div>
 
-              <div className='w-36 border-r-2 pl-4'>
-                <div className='w-16'>청결도</div>
-                <div>4.8</div>
-                <Cleanliness className='mt-12' />
-              </div>
+                <div className='relative  h-[95px] w-36 border-r-2 px-3 xl:w-full xl:border-b-2 xl:border-r-0 xl:p-5 xl:px-0'>
+                  <span className='w-16 xl:absolute xl:bottom-4 xl:left-10'>정확도</span>
+                  <div className='xl:absolute xl:bottom-4 xl:right-0'>4.8</div>
+                  <Accuracy className='absolute bottom-3' />
+                </div>
+                <div className='relative  h-[95px] w-36 border-r-2 px-3 xl:w-full xl:border-b-2 xl:border-r-0 xl:p-5 xl:px-0'>
+                  <span className='w-16 xl:absolute xl:bottom-4 xl:left-10'>체크인</span>
+                  <div className='xl:absolute xl:bottom-4 xl:right-0'>4.8</div>
+                  <CheckIn className='absolute bottom-3' />
+                </div>
 
-              <div className='w-36 border-r-2 pl-4'>
-                <div className='w-16'>정확도</div>
-                <div>4.8</div>
-                <Accuracy className='mt-12' />
-              </div>
-              <div className='w-36 border-r-2 pl-4'>
-                <div className='w-16'>체크인</div>
-                <div>4.8</div>
-                <CheckIn className='mt-12' />
-              </div>
+                <div className='relative  h-[95px] w-36 border-r-2 px-3 xl:w-full xl:border-b-2 xl:border-r-0 xl:p-5 xl:px-0'>
+                  <span className='w-16 xl:absolute xl:bottom-4 xl:left-10'>의사소통</span>
+                  <div className='xl:absolute xl:bottom-4 xl:right-0'>4.8</div>
+                  <Interaction className='absolute bottom-3' />
+                </div>
 
-              <div className='w-36 border-r-2 pl-4'>
-                <div className='w-16'>의사소통</div>
-                <div>4.8</div>
-                <Interaction className='mt-12' />
-              </div>
+                <div className='relative  h-[95px] w-36 border-r-2 px-3 xl:w-full xl:border-b-2 xl:border-r-0 xl:p-5 xl:px-0'>
+                  <span className='w-16 xl:absolute xl:bottom-4 xl:left-10'>위치</span>
+                  <div className='xl:absolute xl:bottom-4 xl:right-0'>4.8</div>
+                  <Location className='absolute bottom-3' />
+                </div>
 
-              <div className='w-36 border-r-2 pl-4'>
-                <div className='w-16'>위치</div>
-                <div>4.8</div>
-                <Location className='mt-12' />
-              </div>
-
-              <div className='w-36 pl-4'>
-                <div>가격 대비 만족도</div>
-                <div>4.8</div>
-                <Satisfication className='mt-12' />
+                <div className='relative  h-[95px] w-36  break-keep pl-3 xl:w-full xl:border-b-2 xl:border-r-0 xl:p-5 xl:px-0'>
+                  <span className='xl:absolute xl:bottom-4 xl:left-10'>가격 대비 만족도</span>
+                  <div className='xl:absolute xl:bottom-4 xl:right-0'>4.8</div>
+                  <Satisfication className='absolute bottom-3' />
+                </div>
               </div>
             </div>
-
-            <div className='-ml-8 w-[1200px] divide-y-2 divide-gray-400'>
-              <div className='mt-20'></div>
-              <div></div>
-            </div>
+            <hr className='mt-2' />
 
             {/* 리뷰 나오는 곳 */}
-            <div>
-              <div className='relative top-8 text-2xl font-semibold'>{`후기 ${reviews.length}개`}</div>
-              <div className='flex justify-end'>
-                <div className='group inline-block'>
-                  <button className='flex h-12 max-w-32 items-center rounded-2xl border bg-white pl-2 font-semibold'>
-                    <p className='pr-1 font-semibold'>{selectedMenu}</p>
-                    <svg
-                      className='h-4 w-4 transform fill-current transition
-                    duration-150 ease-in-out group-hover:-rotate-180'
-                      xmlns='http://www.w3.org/2000/svg'
-                      viewBox='0 0 20 20'
-                    >
-                      <path d='M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z' />
-                    </svg>
+            <div className='w-full flex-col items-center p-5 xl:w-[600px]'>
+              <div className='flex h-[32px] w-full items-center justify-between '>
+                <div className='text-xl font-semibold'>{`후기 ${reviews.length}개`}</div>
+                <div className='relative'>
+                  <button
+                    className='flex h-[32px] w-auto items-center gap-2 rounded-2xl border bg-white px-3 font-semibold'
+                    onClick={() => {
+                      setOnClickSearchMenu(true)
+                    }}
+                    onFocus={() => {
+                      setOnFocusButton(true)
+                    }}
+                    onBlur={(e) => {
+                      if (!e.currentTarget.contains(e.relatedTarget) && !onClickSearchMenu) {
+                        setOnFocusButton(false)
+                      }
+                    }}
+                  >
+                    <p className=' text-xs'>{selectedMenu}</p>
+                    <DownIcon />
                   </button>
                   <div
-                    className='absolute w-40 origin-top-left -translate-x-20 scale-0 transform
-                  rounded-sm border bg-white transition duration-150 group-hover:scale-100'
+                    className={`absolute right-0 w-40 rounded-sm border bg-white ${onClickSearchMenu && onFocusButton ? 'scale-100' : 'scale-0'}`}
                   >
-                    <div>
-                      <ul>
-                        {menus.map((menu, index) => (
-                          <li
-                            className='h-10 w-40 hover:bg-slate-200'
-                            key={index}
-                            onClick={() => handleMenuClick(menu)}
-                          >
-                            {menu}
-                          </li>
-                        ))}
-                      </ul>
-                    </div>
+                    <ul>
+                      <li
+                        className='h-10 w-40 p-3 hover:bg-navigatorOneLayoutColor'
+                        onClick={() => {
+                          setSelectedMenu('최신순')
+                          setOnClickSearchMenu(false)
+                        }}
+                      >
+                        최신순
+                      </li>
+                      <li
+                        className='h-10 w-40 p-3 hover:bg-navigatorOneLayoutColor'
+                        onClick={() => {
+                          setSelectedMenu('높은 평점순')
+                          setOnClickSearchMenu(false)
+                        }}
+                      >
+                        높은 평점순
+                      </li>
+                      <li
+                        className='h-10 w-40 p-3 hover:bg-navigatorOneLayoutColor'
+                        onClick={() => {
+                          setSelectedMenu('낮은 평점순')
+                          setOnClickSearchMenu(false)
+                        }}
+                      >
+                        낮은 평점순
+                      </li>
+                    </ul>
                   </div>
                 </div>
               </div>
-            </div>
-            <div className='mt-12 h-11 w-full rounded-3xl border-2 border-black'>
-              <label className='flex'>
-                <div className='ml-4 mt-3'>
-                  <Search />
-                </div>
+
+              <form
+                action={'http://localhost:8080/api/review/1'}
+                method='get'
+                className='group mt-3 flex h-11 w-[98%] items-center rounded-3xl border border-mainGray px-4 focus-within:border-2'
+              >
+                <Search className=' flex-none' />
+
                 <input
-                  className='ml-2 mt-0 h-10 text-xl focus:outline-none'
+                  className='group ml-2 h-10  w-full text-base text-sm focus:outline-none'
                   type='text'
                   placeholder='후기 검색'
                 />
-              </label>
-            </div>
+              </form>
 
-            <div className='mt-6'>
-              {reviewExist ? (
-                initialReviews.map((eachReview, index) => (
-                  <section key={index} className='flex flex-col md:h-[226px] lg:h-[150px] '>
-                    <div className='mb-2 ml-0 mr-auto flex'>
-                      <div className='mr-[14px] h-full w-12'>
+              <div className='mt-6'>
+                {reviewExist ? (
+                  initialReviews.map((eachReview, index) => (
+                    <section key={index} className='mb-7 flex flex-col gap-3'>
+                      <div className='flex flex-row gap-3'>
                         <div className='relative h-12 w-12'>
                           <Image
                             key={index}
@@ -248,27 +252,30 @@ export default function ReviewModal({
                             className='rounded-full object-cover'
                           />
                         </div>
-                      </div>
-                      <div className='flex flex-col justify-center space-y-[2px]'>
-                        <div className='text-[16px] font-semibold text-mainBlack'>
-                          {`${eachReview.reviewerName}`}
+
+                        <div className='flex flex-col justify-center space-y-[2px]'>
+                          <div className='text-[16px] font-semibold text-mainBlack'>
+                            {`${eachReview.reviewerName}`}
+                          </div>
+                          <div className='text-[14px] text-mainGray'>{`${eachReview.nation}`}</div>
                         </div>
-                        <div className='text-[14px] text-mainGray'>{`${eachReview.nation}`}</div>
                       </div>
-                    </div>
-                    <div className='mb-1 ml-0 mr-auto flex items-center space-x-1'>
-                      <StarRateGenerator score={eachReview.score} />
-                    </div>
-                    <div className='ml-0 mr-auto overflow-hidden'>
-                      <p className='line-break-auto line-height-1.5 h-[72px] w-[450px] break-keep'>
-                        {`${eachReview.content}`}
-                      </p>
-                    </div>
-                  </section>
-                ))
-              ) : (
-                <div>리뷰가 없습니다.</div>
-              )}
+                      <div>
+                        <div className='flex items-center space-x-1'>
+                          <StarRateGenerator score={eachReview.score} />
+                        </div>
+                        <div className='mt-1 overflow-hidden'>
+                          <p className='line-break-auto line-height-1.5 break-keep'>
+                            {`${eachReview.content}`}
+                          </p>
+                        </div>
+                      </div>
+                    </section>
+                  ))
+                ) : (
+                  <div>리뷰가 없습니다.</div>
+                )}
+              </div>
             </div>
           </div>
         </Box>
--- src/components/rooms/room-introduction.tsx
+++ src/components/rooms/room-introduction.tsx
@@ -1,5 +1,7 @@
 'use client'
+import { useState } from 'react'
 import StarIcon from '/public/images/star.svg'
+import ReviewModal from '@/components/rooms/modal/review-modal'
 
 interface Props {
   introduction: string
@@ -8,6 +10,7 @@ interface Props {
   bedCount: number
   bathroomCount: number
   roomReviewTotal: ReviewTotalCount
+  reviews: Review[]
 }
 
 interface ReviewTotalCount {
@@ -15,23 +18,41 @@ interface ReviewTotalCount {
   reviewsAvg: number
 }
 
-export default async function RoomIntroduction({
+interface Review {
+  reviewId: number
+  content: string
+  writeAt: string
+  reviewerName: string
+  reviewerProfileImageUrl: string
+  score: number
+  nation: string
+}
+
+export default function RoomIntroduction({
   introduction,
   guestCapacity,
   bedCount,
   bedroomCount,
   bathroomCount,
   roomReviewTotal,
+  reviews,
 }: Props) {
+  const [reviewModalOpen, setReviewModalOpen] = useState(false)
+  const handleOpen = () => {
+    setReviewModalOpen(!reviewModalOpen)
+    console.log(reviewModalOpen)
+  }
   const result =
     roomReviewTotal.reviewsAvg === 50 ? (
       <div>후기가 없습니다.</div>
     ) : (
-      <div className='flex items-center flex-row pt-2 gap-1 text-base font-semibold '>
+      <div className='flex flex-row items-center gap-1 pt-2 text-base font-semibold '>
         <StarIcon style={{ width: '16px', height: '16px' }} />
         <span className='flex items-center '>{roomReviewTotal.reviewsAvg}</span>
         <span>·</span>
-        <button className='text-gray-900 underline'>후기 {roomReviewTotal.reviewsCount}개</button>
+        <button className='text-gray-900 underline' onClick={handleOpen}>
+          후기 {roomReviewTotal.reviewsCount}개
+        </button>
       </div>
     )
 
@@ -47,16 +68,21 @@ export default async function RoomIntroduction({
           <section>
             <div className='flex flex-col'>
               <div className='flex flex-row'>
-                <span className='text-gray-900 text-xl font-semibold'>{introduction}</span>
+                <span className='text-xl font-semibold text-gray-900'>{introduction}</span>
               </div>
               <div className='flex flex-row pt-1'>
-                <span className='text-gray-900 text-base flex'>
+                <span className='flex text-base text-gray-900'>
                   {bedCount > 0
                     ? `최대 인원${guestCapacity}명, 침대 ${bedCount}개, 욕실 ${bathroomCount}개`
                     : `최대 인원${guestCapacity}명, 침실 ${bedroomCount}개, 욕실 ${bathroomCount}개`}
                 </span>
               </div>
               {result}
+              <ReviewModal
+                reviews={reviews}
+                reviewModalOpen={reviewModalOpen}
+                setReviewModalOpen={setReviewModalOpen}
+              />
             </div>
           </section>
         </div>
