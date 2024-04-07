2df8fa6c6961f6ec0d774a0e666520bd4c21164f
“Winsomejoo” <tmdwn0603@naver.com>
2024-04-07 16:29:02 +0900

    feat:userInformation 컴포넌트 Backend 연결

--- src/app/users/show/[id]/page.tsx
+++ src/app/users/show/[id]/page.tsx
@@ -1,5 +1,5 @@
 import Authinfo from '@/components/profile/authinfo'
-import Identification from '@/components/profile/identification'
+import Information from '@/components/profile/information'
 import Introduction from '@/components/profile/introduction'
 import IsAuthinfo from '@/components/profile/isAuthinfo'
 import Review from '@/components/profile/review'
@@ -16,7 +16,7 @@ export default async function UserProfilePage({ params: { id } }: IdParams) {
   return (
     <section className='relative flex top-[40px] flex-wrap left-[100px]'>
       <div className='relative left-[200px] md:w-1/3 mb-4 md:mb-0'>
-        <Identification id={usersData.name} />
+        <Information id={id} />
 
         {usersData.isAuth === true ? (
           <IsAuthinfo name={usersData.name} />
