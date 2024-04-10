6b2e04328d9aed99706fc40062b7b928ff362d94
wjdsheep15 <wjd15sheep@gmail.com>
2024-04-10 20:41:54 +0900

    Feat: Add review Search

--- src/app/api/room/[id]/review/route.js
+++ src/app/api/room/[id]/review/route.js
@@ -1,12 +1,33 @@
-import reviewsData from '../../../../../dummyDatas/reviewsData.json'
 /**
  *
  * @param {*} roomId
- * @returns 
+ * @returns Response data
  */
-export async function GET(request, { params }) {
-  const id = parseInt(params.id);
-  const reviews = reviewsData.Review.filter((review) => review.roomId === id);
 
-  return Response.json({ data: reviews }, { status: 200 });
+export async function POST(request, { params }) {
+  const formData = await request.formData()
+  const body = Object.fromEntries(formData)
+  const id = params.id
+
+  const result = await fetch(`http://localhost:8080/api/review/reviewSearch/${id}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(body),
+  })
+
+  const inner = await result.json()
+
+  if (inner.code !== 0) {
+    inner.result = []
+  }
+  const reviewSearchData = inner.result
+
+  return new Response(JSON.stringify({ data: reviewSearchData }), {
+    status: 200,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
 }
--- src/app/api/route.js
+++
@@ -1,5 +0,0 @@
-import categoryData from '../../dummyDatas/categoriesData.json'
-
-export async function GET() {
-  return Response.json({ data: categoryData.Category })
-}
